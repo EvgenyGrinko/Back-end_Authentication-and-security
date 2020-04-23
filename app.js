22c75015d03c501a2062305e4dabce78a7b5f91e
Evgeniy Grinko <john_forever_young@mail.ru>
2020-04-23 16:07:15 +0300

    Level 3 - Add hashing for passwords
    Instead encryption added hash function. Installed  module "md5" which provides corresponding algorithm.
    The password after registration converts by hash function into a new hash-code and stored in database in that state.
    When the user tries to login, his typed password also hashed and searched in database for similar hash-code.
    This approach is available, because for the same sequence of characters, hashing algorithm generates the same hash code.
    Note: MD5 algorithm is absolutely insecure and can't be used in real projects.

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');//It automatically encryptes specified fields
-//when we call "save()" method and decryptes when we call "find()". 
+const md5 = require('md5');//a JS module for hashing messages with MD5 algorithm. MD5 algorithm is not secure
 
 const app = express();
 
@@ -15,18 +14,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect('mongodb://localhost:27017/usersDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
-//Remember: encryption should be done for the Schema before you will create based on it collection (bofore "mongoose.model()")
-
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-const secretSequence = process.env.SECRETSEQUENCE; //A key for encryption/decryption
-//File "process.env" object is created automatically by "dotenv" module from ".env" file and keeps all our environment variables 
-
-//There is no need to encrypt all data, as we will look for certain files in DB using unencrypted field "email"
-userSchema.plugin(encrypt, { secret: secretSequence, encryptedFields: ['password'] });
 
 const User = new mongoose.model('User', userSchema);
  
@@ -45,7 +36,7 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if(err) {
@@ -59,7 +50,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email: username}, function(err, foundUser){
         if (err) {
             console.log(err);
